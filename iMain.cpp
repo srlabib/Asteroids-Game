df04c8b7257eeaa0b635f7a03634986833ccbe6e
srlabib <srlabib99@gmail.com>
2024-11-07 23:11:29 +0600

    updates

--- iMain.cpp
+++ iMain.cpp
@@ -1,16 +1,22 @@
 # include "iGraphics.h"
+#include <stdlib.h>
+#include "structures.cpp"
 
-int x = 300, y = 300, r = 20;
+
+gameObject spaceship;
+object_properties player;
+
+
+
+void inititalize_gameObjects(gameObject *object, char filename[]);
+void Draw_gameObject(object_properties player);
 
 void iDraw() {
-	//place your drawing codes here
 	iClear();
-	iSetColor(20, 200, 200);
-	iFilledCircle(x, y, r);
-	//iFilledRectangle(10, 30, 20, 20);
-	iSetColor(20, 200, 0);
-	iText(40, 40, "Hi, I am iGraphics");
+	Draw_gameObject(player);
 }
+
+
 
 /*
 	function iMouseMove() is called when the user presses and drags the mouse.
@@ -29,13 +35,11 @@ void iMouse(int button, int state, int mx, int my) {
 	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
 		//place your codes here
 		//	printf("x = %d, y= %d\n",mx,my);
-		x += 10;
-		y += 10;
+
 	}
 	if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
 		//place your codes here
-		x -= 10;
-		y -= 10;
+
 	}
 }
 
@@ -69,7 +73,76 @@ void iSpecialKeyboard(unsigned char key) {
 
 
 int main() {
+	
 	//place your own initialization codes here.
-	iInitialize(400, 400, "demo");
+	inititalize_gameObjects(&spaceship,"assets/spaceship.txt");
+	/* //DEGUB initialization
+	printf("%d\n",spaceship.number_of_polygons);
+	for(int i = 0; i<spaceship.number_of_polygons; i++){
+		printf("number of nodes: %d\n",spaceship.size[i]);
+		printf("color code: %d %d %d\n",spaceship.color[i][0],spaceship.color[i][1],spaceship.color[i][2]);
+		for(int j = 0; j<spaceship.size[i];j++){
+			printf("%lf  %lf\n",spaceship.x[i][j],spaceship.y[i][j]);
+		}
+	}
+	*/
+	player.position.x = 500;
+	player.position.y = 500;
+	player.object = spaceship;
+	iInitialize(1080, 720, "demo");
 	return 0;
+}
+
+
+void inititalize_gameObjects(gameObject *object, char filename[]){
+	FILE *gameData;
+	gameData = fopen(filename,"r");
+	
+	if(gameData == NULL){
+		perror("File opening failed:\n");
+		exit(EXIT_FAILURE);
+	}
+
+
+	fscanf(gameData,"%d",&object->number_of_polygons);
+
+	object->x = (double**)malloc(sizeof(double*)*object->number_of_polygons);
+	object->y = (double**)malloc(sizeof(double*)*object->number_of_polygons);
+	object->size = (int *)malloc(sizeof(int)*object->number_of_polygons);
+	object->color = (int (*)[3])malloc(sizeof(int [3])*object->number_of_polygons);
+
+	for(int i = 0; i<object->number_of_polygons; i++){
+
+		fscanf(gameData,"%d",&object->size[i]);
+		fscanf(gameData,"%d %d %d",&object->color[i][0],&object->color[i][1],&object->color[i][2]);
+
+		object->x[i] = (double *)malloc(sizeof(double)*object->size[i]);
+		object->y[i] = (double *)malloc(sizeof(double)*object->size[i]);
+
+		for(int j = 0; j<object->size[i]; j++){
+			fscanf(gameData,"%lf",&object->x[i][j]);
+		}
+		for(int j = 0; j<object->size[i]; j++){
+			fscanf(gameData,"%lf",&object->y[i][j]);
+		}
+
+	}
+	fclose(gameData);
+
+}
+
+
+void Draw_gameObject(object_properties player){
+	for(int i = 0; i<player.object.number_of_polygons; i++){
+		iSetColor(player.object.color[i][0],player.object.color[i][1],player.object.color[i][2]);
+		double X[player.object.size[i]];
+		double Y[player.object.size[i]];
+		for(int j = 0; j<player.object.size[i]; j++){
+			X[j] = player.object.x[i][j] + player.position.x;
+			Y[j] = player.object.y[i][j] + player.position.y;
+			
+		}
+		iFilledPolygon(X,Y,player.object.size[i]);
+
+	}
 }
---
+++ structures.cpp
@@ -0,0 +1,21 @@
+// #include<stdio.h>
+
+struct vector2
+{
+	double x;
+	double y;
+};
+
+struct gameObject{
+	int number_of_polygons;
+	double **x;  // x coordinate of all polygons
+	double **y;  // y coordinate of all polygons
+	int *size; // number of nodes of each polygons
+	int (*color)[3];
+};
+
+struct object_properties{
+	gameObject object;
+	vector2 position;
+	vector2 velocity;
+};
